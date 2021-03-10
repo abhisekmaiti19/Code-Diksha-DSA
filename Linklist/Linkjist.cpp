3340704dce30cb9189dfe0759bacdf22bf875294
abhisekmaiti19 <63227192+abhisekmaiti19@users.noreply.github.com>
2021-03-10 16:53:35 +0530

    adding new files

---
+++ Linklist/Linkjist.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Listnode {
+
+	public:
+		int val;
+		Listnode* next;
+
+		Listnode (int val)
+		{
+			this->val = val;
+			this->next = NULL;
+		}
+
+}
+
+int main()
+{
+	int 
+	int *ptr = &x
+}
