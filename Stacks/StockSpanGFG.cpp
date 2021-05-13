ab7da7d55050977453d7a307947dcd05becd2c06
abhisekmaiti19 <63227192+abhisekmaiti19@users.noreply.github.com>
2021-05-13 19:43:17 +0530

    update stock span

--- Stacks/StockSpanGFG.cpp
+++ Stacks/StockSpanGFG.cpp
@@ -1,2 +1,35 @@
 //reffer this link = https://www.geeksforgeeks.org/the-stock-span-problem/
-#include<
+#include<bits/stdc++.h>
+using namespace std;
+
+void stockSpan(int arr[],int n)
+{
+	stack<int> s;
+	 
+	for(int i=0;i<n;i++)
+	{
+		while(!s.empty() and arr[s.top()]<arr[i])
+		{
+			s.pop();
+		}
+		int days;
+		if(!s.empty())
+		{
+			 days = i - s.top();
+		}
+		else
+		{
+			days++;
+		}
+		
+		cout<<arr[i]<<" -> "<<days<<endl;
+		s.push(i);
+	}
+
+
+}
+int main ()
+{
+	int arr [5]= {100,44,80,90,30};
+	stockSpan(arr,5);
+}
